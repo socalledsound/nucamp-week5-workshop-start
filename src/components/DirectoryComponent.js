27b1efb1f4ebf9eec7a46dab6d35a336e8d9af5b
Chris Kubick <socalledsound@gmail.com>
2021-09-18 08:51:01 -0700

    cleaning up a bit

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,69 +1,31 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { Loading } from './LoadingComponent';
-import { baseUrl } from '../shared/baseUrl';
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite}) {
-    return (
-        <Card>
-            <Link to={`/directory/${campsite.id}`}>
-                <CardImg width="100%" src={baseUrl + campsite.image} alt={campsite.name} />
-                <CardImgOverlay>
-                    <CardTitle>{campsite.name}</CardTitle>
-                </CardImgOverlay>
-            </Link>
-        </Card>
-    );
-}
+class Directory extends Component {
 
-function Directory(props) {
+    render() {
+        console.log(this.props.campsites)
+        const directory = this.props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
 
-    const directory = props.campsites.campsites.map(campsite => {
-        return (
-            <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite={campsite} />
-            </div>
-        );
-    });
-
-    if (props.campsites.isLoading) {
         return (
             <div className="container">
                 <div className="row">
-                    <Loading />
+                    {directory}
                 </div>
             </div>
         );
     }
-    if (props.campsites.errMess) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <h4>{props.campsites.errMess}</h4>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Directory</BreadcrumbItem>
-                    </Breadcrumb>
-                    <h2>Directory</h2>
-                    <hr />
-                </div>
-            </div>
-            <div className="row">
-                {directory}
-            </div>
-        </div>
-    );
 }
 
 export default Directory;
